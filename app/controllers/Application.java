80c818a0a88f02777266d4984e95379d3d4c0984
unknown <C813507@W208701.rails.rwy.bnsf.com>
2014-02-25 09:29:21 -0600

    Inital Version

---
+++ app/controllers/Application.java
@@ -0,0 +1,32 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import java.util.*;
+
+import models.*;
+
+public class Application extends Controller {
+
+    public static void index() {
+        render();
+    }
+    
+    public static void JQuery1(){
+    	render();
+    }
+    
+    public static void hoff(){
+    	render();
+    }
+    
+    public static void jump(){
+    	render();
+    }
+    
+    public static void html5(){
+    	render();
+    }
+
+}
